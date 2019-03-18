40b3865fa2d449d7ad807d11ff2026386971cc7c
xiasm <xiasem@163.com>
2019-03-18 23:02:50 +0800

    del code

--- host/src/main/java/com/xsm/dlapkimitate/MainActivity.java
+++ host/src/main/java/com/xsm/dlapkimitate/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestPermission();
-//        DLManager.getInstance().setContext(this);
         initView();
     }
 
